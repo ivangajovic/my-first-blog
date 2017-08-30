1a0b574db4998e59b85ea2b285d058b2ffbe4b7e
Ivan <mt.ivangajovic@gmail.com>
2017-08-30 14:07:49 +0200

    Added draft and delete

--- myblog/views.py
+++ myblog/views.py
@@ -9,9 +9,11 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'myblog/post_list.html', {'posts':posts})
 
+
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'myblog/post_detail.html', {'post':post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -19,12 +21,12 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'myblog/post_edit.html', {'form':form})
+
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -33,9 +35,24 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
             form = PostForm(instance=post)
     return render(request, 'myblog/post_edit.html', {'form':form})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('create_date')
+    return render(request, 'myblog/post_draft_list.html', {'posts':posts})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
